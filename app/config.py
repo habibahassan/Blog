6d2ea19555b64f716650065b3cf5b4111550e72b
habibahassan <halimaadan92@gmail.com>
2020-02-20 11:33:02 +0300

    added database url to appconfig.py

--- app/config.py
+++ app/config.py
@@ -1,29 +1,29 @@
-# import os
+import os
 
-# class Config:
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://habbiba:zully1234@localhost/blog'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://habbiba:zully1234@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-#     UPLOADED_PHOTOS_DEST ='app/static/img'
+    UPLOADED_PHOTOS_DEST ='app/static/img'
 
-# class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://habbiba:zully1234@localhost/blog'
-# class DevConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://habbiba:zully1234@localhost/blog'
-#     DEBUG = True
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://habbiba:zully1234@localhost/blog'
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://habbiba:zully1234@localhost/blog'
+    DEBUG = True
 
-# class ProdConfig(Config):
-#     '''
-#     Production configuration child class
-#     Args:
-#         Config:The parent configuration class with General configuration settings
-#     '''
-#     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://habbiba:zully1234@localhost/blog'
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://habbiba:zully1234@localhost/blog'
     
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig,
-# 'test':TestConfig
-# }
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
