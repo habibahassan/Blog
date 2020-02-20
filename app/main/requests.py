3c6bd64c4dd7c1a68529253ec60184bcfec73ae6
habibahassan <halimaadan92@gmail.com>
2020-02-20 11:27:15 +0300

    added quote function request on requests.py

--- app/main/requests.py
+++ app/main/requests.py
@@ -1,12 +1,12 @@
-# import urllib.request,json
+import urllib.request,json
 
-# def get_quotes():
-#    '''
-#    Function that gets the json response to our url request
-#    '''
-#    get_quotes_url = 'http://quotes.stormconsultancy.co.uk/random.json'
-#    with urllib.request.urlopen(get_quotes_url) as url:
-#        quotes = url.read()
-#        get_sources_response = json.loads(quotes)
-#        print(quotes)
-#    return get_sources_response
+def get_quotes():
+   '''
+   Function that gets the json response to our url request
+   '''
+   get_quotes_url = 'http://quotes.stormconsultancy.co.uk/random.json'
+   with urllib.request.urlopen(get_quotes_url) as url:
+       quotes = url.read()
+       get_sources_response = json.loads(quotes)
+       print(quotes)
+   return get_sources_response
